3af0b74dd5b1f5ae2e7e593aab47fb7828e1fdd0
Andre Le <logisticx@yahoo.com>
2022-04-24 00:34:49 +0000

    Completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,20 @@
+
+var person = {
+
+  firstName: 'Andre',
+  lastName: 'Le',
+  hobby: 'Photography'
+};
+console.log(person);
+
+var fullName = ('Andre' + ' ' + 'Le');
+
+console.log('The persons name is:', fullName);
+
+person.job = 'Audio Video Technician';
+
+console.log('This persons current job is:', person.job);
+
+person.previousJob = 'student';
+
+console.log('This persons previous job is:', person.previousJob);
