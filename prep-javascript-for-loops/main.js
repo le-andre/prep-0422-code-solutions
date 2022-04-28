eab0fe7e0365989848345a7a5386ef6426bbd6c9
Andre Le <logisticx@yahoo.com>
2022-04-28 22:05:34 +0000

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (let m = 0; m < 10; m++) {
+  console.log(m);
+}
+
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
+}
+
+for (let n = 100; n > 0; n--) {
+  console.log('Time till explosion:', n);
+}
